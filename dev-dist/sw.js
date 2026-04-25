3678ecf138b1708e38a3367121fc9d3f44da86b5
DarkWolf260 <ruben_eduardo2003@outlook.com>
2026-04-25 11:06:44 -0400

    feat: add AlertDialog component and update service worker configuration

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.g7t19nk5u4o"
+    "revision": "0.k5kmkhcftf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
